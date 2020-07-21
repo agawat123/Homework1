8691456a10974820d055996b8fe12d1d9e6096f8
Agawat Sithisakulrut <ank5696@gmail.com>
2020-07-21 21:33:22 +0700

    first commit

---
+++ homework_1/homework_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"1st Term of 2020 \t\\45132114   Wanthanee Prachuabsupakij"<<endl;
+	cout<<endl;
+	cout<<"--------------------------------------------------------------------------------------------------"<<endl;
+	cout<<"|  Subject ID | Subject name                   | Score (100) | Grad |"<<endl;
+	cout<<"--------------------------------------------------------------------------------------------------"<<endl;
+	cout<<"| 4570034     | Introduction to computer        |     85      |\t\"A\" | "<<endl;
+	cout<<"| 4580247     | English                         |     74      |\t\"B\" | "<<endl;
+	cout<<"| 4530001     | Design Thinking                 |     92      |\t\"A\" | "<<endl;
+	cout<<"--------------------------------------------------------------------------------------------------"<<endl;
+	cout<<"|  TOTAL      | Subject = 3                     | Average = 3.87  |"<<endl;
+	cout<<"=================================*===";
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"KMUTNB"<<endl;
+
+	
+	
+	return(0);
+
+}
